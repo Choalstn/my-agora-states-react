1c3a9d6a2d40948f3cd82b30c953c71023ea00fa
조민수 <choalstn@gmail.com>
2023-02-11 02:29:52 +0900

    create 기능 구현 (리렌더링 오류 있음)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import DisucussionContainer from "./Discussions_Wrapper";
+import Modal from "./Modal.js";
 import axios from "axios";
 
 function App() {
   const [discussion, setDiscussion] = useState();
   const [loading, setLoading] = useState(true);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  const setData = (data) => {
+    setDiscussion(data);
+  };
 
   useEffect(() => {
     axios.get("http://localhost:4000/discussions/").then((res) => {
-      setDiscussion(res.data);
+      setData(res.data);
       setLoading(false);
     });
   }, []);
@@ -23,9 +36,17 @@ function App() {
           <div className="form__container">
             <h1>My Agora States</h1>
 
-            <h1 className="popup_open_btn">질문 등록하기</h1>
+            <h1 id="popup_open_btn" onClick={openModal}>
+              질문 등록하기
+            </h1>
           </div>
 
+          <Modal
+            open={modalOpen}
+            close={closeModal}
+            header="질문하기"
+            data={setData}
+          />
           <DisucussionContainer data={discussion} />
         </>
       )}
---
+++ src/Modal.js
@@ -0,0 +1,117 @@
+import React, { useState } from "react";
+import "./modal.css";
+import "./App.css";
+import axios from "axios";
+
+const Modal = (props) => {
+  // 열기, 닫기, 모달 헤더 텍스트를 부모로부터 받아옴
+  const { open, close, header, data } = props;
+  const [submit, setSubmit] = useState(false);
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [content, setContent] = useState("");
+  const [discussion, setDiscussion] = useState();
+  const date = new Date();
+
+  const handelAuthor = (e) => {
+    setAuthor(e.target.value);
+  };
+
+  const handelTitle = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handelContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const newDisucussion = () => {
+    axios
+      .post("http://localhost:4000/discussions/submit", {
+        id: "105564451",
+        createdAt: date.toLocaleDateString("ko-kr"),
+        title: title,
+        url: "",
+        author: author,
+        answer: null,
+        bodyHTML: content,
+        avatarUrl: "https://avatars.githubusercontent.com/u/105564451?v=4",
+      })
+      .then((res) =>
+        axios.get(
+          "http://localhost:4000/discussions".then((res) => {
+            data(res.data);
+          })
+        )
+      );
+
+    close();
+  };
+
+  return (
+    // 모달이 열릴때 openModal 클래스가 생성된다.
+    <div className={open ? "openModal modal" : "modal"}>
+      {open ? (
+        <section>
+          <header>
+            {header}
+            <button className="close" onClick={close}>
+              x
+            </button>
+          </header>
+
+          <main>
+            <form className="form">
+              <div className="form__input--wrapper">
+                <div className="form__input--name">
+                  <label htmlFor="name">Enter your name: </label>
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    required
+                    onChange={handelAuthor}
+                  />
+                </div>
+                <br />
+                <div className="form__input--title">
+                  <label htmlFor="name">Enter your title: </label>
+                  <input
+                    type="text"
+                    name="name"
+                    id="title"
+                    required
+                    onChange={handelTitle}
+                  />
+                </div>
+                <br />
+                <div className="form__textbox">
+                  <label htmlFor="story">Your question: </label>
+                  <textarea
+                    id="story"
+                    name="story"
+                    placeholder="질문을 작성하세요"
+                    required
+                    onChange={handelContent}
+                  ></textarea>
+                </div>
+                <br />
+              </div>
+            </form>
+          </main>
+
+          <footer>
+            <button className="submit" onClick={newDisucussion}>
+              submit
+            </button>
+            <button className="close" onClick={close}>
+              close
+            </button>
+          </footer>
+        </section>
+      ) : null}
+    </div>
+  );
+};
+
+export default Modal;
